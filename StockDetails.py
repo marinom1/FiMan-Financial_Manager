2c8a2df2d3919c0300a9a7532d18a6bbb86f69fa
Ian <i.seto00@gmail.com>
2021-03-14 19:15:30 -0400

    Separated StockMarket.py into StockDetails.py and StockNews.py

--- StockDetails.py
+++ StockDetails.py
@@ -1,1 +1,53 @@
 import requests
+from config import PolygonIOKey
+
+# Ticker Details
+def getTicker(tickerList):
+    for ticker in tickerList:
+        print('Ticker:', ticker)
+        details = f'https://api.polygon.io/v1/meta/symbols/{ticker}/company?&apiKey={PolygonIOKey}'
+
+        responseDetails = requests.get(details)
+        detailsJSON = responseDetails.json()
+
+        print('Ticker Details:')
+        # print(detailsJSON)
+
+        def displayDetails():
+            logo = detailsJSON['logo']
+            country = detailsJSON['hq_country']
+            industry = detailsJSON['industry']
+            sector = detailsJSON['sector']
+            marketcap = detailsJSON['marketcap']
+            ceo = detailsJSON['ceo']
+            url = detailsJSON['url']
+            description = detailsJSON['description']
+            name = detailsJSON['name']
+            symbol = detailsJSON['symbol']
+            state = detailsJSON['hq_state']
+            tags = detailsJSON['tags'][0]
+
+            print('Name:', name)
+            print('State:', state)
+            print('Country:', country)
+            print('Symbol:', symbol)
+            print('Sector:', sector)
+            print('Industry:', industry)
+            print('Description:', description)
+            print('Market Cap:', marketcap)
+            print('CEO:', ceo)
+            print('URL:', url)
+            print('Tags:', tags)
+            print('')
+
+        displayDetails()
+
+def main():
+    enterTickers = input('Enter your preferred stock tickers (FB AMZN APPL NFLX GOOGL etc.): ')
+    enterTickers = enterTickers.upper()
+    tickerList = enterTickers.split()
+    # print(tickerList)
+    print('Generating details for selected tickers...')
+    getTicker(tickerList)
+
+main()
--- StockNews.py
+++ StockNews.py
@@ -1,6 +1,33 @@
 import requests
-from operator import itemgetter
 from config import PolygonIOKey
+
+# Ticker News
+def getTicker(tickerList):
+    for ticker in tickerList:
+        print('Ticker:', ticker)
+        news = f'https://api.polygon.io/v1/meta/symbols/{ticker}/news?perpage=1&page=1&apiKey={PolygonIOKey}'
+
+        responseNews = requests.get(news)
+        newsJSON = responseNews.json()
+
+        print('Ticker News:')
+
+        def displayArticles():
+            for i in range(len(newsJSON)):
+                title = newsJSON[i]['title']
+                url = newsJSON[i]['url']
+                source = newsJSON[i]['source']
+                summary = newsJSON[i]['summary']
+
+                print('Title:', title)
+                print('URL: ', url)
+                print('Source:', source)
+                print('Summary: ', summary)
+
+                print('')
+                print('Next Article')
+
+        displayArticles()
 
 def main():
     enterTickers = input('Enter your preferred stock tickers (FB AMZN APPL NFLX GOOGL etc.): ')
@@ -9,31 +36,5 @@ def main():
     # print(tickerList)
     print('Generating news articles for selected tickers...')
     getTicker(tickerList)
-
-# Ticker Details and News
-def getTicker(tickerList):
-    for ticker in tickerList:
-        print('Ticker:', ticker)
-        details = f'https://api.polygon.io/v1/meta/symbols/{ticker}/company?&apiKey={PolygonIOKey}'
-        news = f'https://api.polygon.io/v1/meta/symbols/{ticker}/news?perpage=1&page=1&apiKey={PolygonIOKey}'
-
-        responseDetails = requests.get(details)
-        responseNews = requests.get(news)
-        detailsJSON = responseDetails.json()
-        newsJSON = responseNews.json()
-
-        for i in range(len(newsJSON)):
-            title = newsJSON[i]['title']
-            url = newsJSON[i]['url']
-            source = newsJSON[i]['source']
-            summary = newsJSON[i]['summary']
-
-            print('Title:', title)
-            print('URL: ', url)
-            print('Source:', source)
-            print('Summary: ', summary)
-
-            print('')
-            print('Next Article')
 
 main()
